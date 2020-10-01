23cb22c247976d8cc4c1f5436b1b245629010cfd
Arleca <carolsantos2002@gmail.com>
2020-10-01 14:02:55 -0300

    Adaptação final dos métodos set_rec_minimo e set_rec_maximo

--- servidor.py
+++ servidor.py
@@ -24,8 +24,8 @@ def inserir_jogo():
     Jogo.create(Placar=placar_atual,
                 MinimoDaTemp = set_minimo_temporada(int(placar_atual)),
                 MaximoDaTemp = set_maximo_temporada(int(placar_atual)),
-                QuebraRecMinimo = set_quebra_recorde_minimo(int(placar_atual)), 
-                QuebraRecMaximo = set_quebra_recorde_maximo(int(placar_atual)))
+                QuebraRecMinimo = set_rec_minimo(int(placar_atual)), 
+                QuebraRecMaximo = set_rec_maximo(int(placar_atual)))
     
     return redirect("/listar_jogos")
 
@@ -65,30 +65,41 @@ def set_maximo_temporada(Placar):
             lista_placares.append(MaximoDaTemp)
             return MaximoDaTemp
 
-count = 0 
+lista_recordes_max=[]
+lista_recordes_min=[]
+jogos = Jogo.select() 
+for rec in jogos:
+    lista_recordes_max.append(rec.QuebraRecMaximo) 
+    lista_recordes_min.append(rec.QuebraRecMinimo)
 
-def set_quebra_recorde_minimo(Placar):
-    QuebraRecMinimo = 0
-    for j in jogos: 
+def set_rec_maximo(Placar):
+    for j in jogos:
+        if max(lista_placares[:-1]) < Placar:
+            QuebraRecMaximo = max(lista_recordes_max) + 1
+            lista_recordes_max.append(QuebraRecMaximo)
+            return QuebraRecMaximo
+        else:
+            for r in lista_placares:
+                if r == Placar:
+                    QuebraRecMaximo = max(lista_recordes_max)
+                    return QuebraRecMaximo
+                else:
+                    QuebraRecMaximo = max(lista_recordes_max)
+                    return QuebraRecMaximo
+
+def set_rec_minimo(Placar):
+    for j in jogos:
         if min(lista_placares[:-2]) > Placar:
-            QuebraRecMinimo = 1
-            print(lista_placares)
+            QuebraRecMinimo = max(lista_recordes_min) + 1
+            lista_recordes_min.append(QuebraRecMinimo)
             return QuebraRecMinimo
         else:
-            QuebraRecMinimo = 0
-            print(lista_placares)
-            return QuebraRecMinimo
-
-
-def set_quebra_recorde_maximo(Placar):
-    QuebraRecMaximo = 0
-    for j in jogos: 
-        if max(lista_placares[:-1]) < Placar or max(lista_placares[:-1]) == Placar:
-            QuebraRecMaximo = 1
-            return QuebraRecMaximo
-        else:
-            QuebraRecMaximo = 0
-            return QuebraRecMaximo
-
+            for r in lista_placares:
+                if r == Placar:
+                    QuebraRecMinimo = max(lista_recordes_min)
+                    return QuebraRecMinimo
+                else:
+                    QuebraRecMinimo = max(lista_recordes_max)
+                    return QuebraRecMinimo
 
 app.run(debug=True)
